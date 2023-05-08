2f0a67ff95d6552c7469467481d85a94dd579b02
Bruno Gomes <brunoshy@gmail.com>
2023-05-08 10:14:15 -0300

    csgo: create lt.cfg

---
+++ csgo/gitpull-cfg.ps1
@@ -0,0 +1,11 @@
+
+
+
+
+# Path to your CS:GO folder
+$PathCSGO = "D:\Games\SteamLibrary\steamapps\common\Counter-Strike Global Offensive"
+
+$PathCFG = "$PathCSGO\csgo\cfg"
+
+cd $($PathCFG)
+wget https://github.com/pobruno/lorthe-games-cfg/blob/main/CSGO/lt.cfg
